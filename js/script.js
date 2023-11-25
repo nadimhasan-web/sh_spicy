b0e12184c0a89c08ce3adc175f3205e88c1bfdae
nadim <nadimhasannayan77@gmail.com>
2023-11-25 12:06:07 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+//offer card slider
+
+$('.offer_card_main').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows:false,
+    autoplaySpeed: 1500,
+    dots:true,
+  });
+
+//menu slider
+ 
+$('.menu_one').slick({
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  autoplay: true,
+  arrows:false,
+  autoplaySpeed: 3000,
+  dots:false,
+});
+
+
+      
+          
